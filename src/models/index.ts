36aa26d0ed336c6349a959e181b2e252f8f559d4
DESKTOP-ELGSLMU\Fukushiro <jpflc2301@gmail.com>
2022-08-05 16:30:40 -0300

    build :

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,12 +5,12 @@ import { configuration } from "../services/db.service";
 config();
 let Database: Sequelize;
 
-// if (process.env.ENV === "DEV") {
-//   Database = new Sequelize(configuration);
-// } else if (process.env.ENV === "PROD") {
-//   if (process.env.DATABASE_URL != undefined) {
-Database = new Sequelize(process.env.DATABASE_URL);
-//   }
-// }
+if (process.env.ENV === "DEV") {
+  Database = new Sequelize(configuration);
+} else if (process.env.ENV === "PROD") {
+  if (process.env.DATABASE_URL != undefined) {
+    Database = new Sequelize(process.env.DATABASE_URL, { dialect: "postgres" });
+  }
+}
 
 export { Database };
